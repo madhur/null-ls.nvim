4153c4533c29105b6ddf63e8d422802ae5822910
Madhur <madhur.ahuja@games24x7.com>
2023-09-27 08:17:30 +0530

    feat: yamllint fix

--- lua/null-ls/helpers/diagnostics.lua
+++ lua/null-ls/helpers/diagnostics.lua
@@ -99,14 +99,12 @@ local from_pattern = function(pattern, groups, overrides)
 
     return function(line, params)
         local results = { line:match(pattern) }
-        log:error(results)
+        log:error(line:match(pattern))
         local entries = {}
 
         for i, match in ipairs(results) do
             entries[groups[i]] = match
         end
-        log:error(entries)
-        log:error(attr_adapters)
 
         return make_diagnostic(entries, defaults, attr_adapters, params, offsets)
     end
